d877d0e6c10089535416b84d8c775af215a4247b
Default user <juanmanueljimenezvergara9@gmail.com>
2022-09-08 20:08:56 +0000

    c++ origin

---
+++ .ipynb_checkpoints/JimenezJuan_S5C2_repasoC-checkpoint.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* Corchetes en c++ camba a indentacion en python
+#cout equivalente a print en la consola
+#recordar colocar ;
+
+#funcion main retorna un entero, en este caso 0*/
+
+int main(){
+    cout << "Hello World";
+    return 0;
+    }
+    
+/* Mas control en el codigo (por ejemplo, en la variable)*/
--- JimenezJuan_S5C2_repasoC.cpp
+++ JimenezJuan_S5C2_repasoC.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
-using namespaces std;
+using namespace std;
 
-#Corchetes en c++ camba a indentacion en python
-#cout equivalente a print en la consola
-#recordar colocar ;
+/* Corchetes en c++ cambia a indentacion en python
+cout equivalente a print en la consola
+recordar colocar ;
 
-#funcion main retorna un entero, en este caso 0
+funcion main retorna un entero, en este caso 0*/
 
 int main(){
     cout << "Hello World";
     return 0;
     }
     
-# Mas control en el codigo (por ejemplo, en la variable)
+/* Mas control en el codigo (por ejemplo, en la variable)
+tipos de datos en c++*/
+
+//en la funcion principal uno llama las otras
