1be3f60d2c4393ef54a6deff2916d3b426cbbfa4
Default user <juanmanueljimenezvergara9@gmail.com>
2022-09-27 21:09:35 +0000

    S7C1_1

--- S7C1/.ipynb_checkpoints/Makefile_JuanJimenez_S7C1_EDO2-checkpoint.mk
+++ S7C1/.ipynb_checkpoints/Makefile_JuanJimenez_S7C1_EDO2-checkpoint.mk
@@ -1,10 +1,20 @@
 #JimenezJuan_S7C1.mk
     
-all: EULER_1.png yeux.txt yeuv.txt xeu.txt a.exe
+all: EULER_1.png Runge_Kutta_1.PNG yeux.txt yeuv.txt xeu.txt a.exe yrkv.txt yrkx.txt
 
-EULER_1.png : PLOTS_JUanJimenez_S7C1_EDO2.py yeux.txt yeuv.txt xeu.txt
+Runge_Kutta_1.PNG : PLOTS_JUanJimenez_S7C1_EDO2.py yeux.txt yeuv.txt xeu.txt yrkv.txt yrkx.txt
 	python PLOTS_JUanJimenez_S7C1_EDO2.py
-    
+	
+EULER_1.png : PLOTS_JUanJimenez_S7C1_EDO2.py yeux.txt yeuv.txt xeu.txt yrkv.txt yrkx.txt
+	python PLOTS_JUanJimenez_S7C1_EDO2.py
+
+
+yrkv.txt : a.exe
+	./a.exe
+
+yrkx.txt : a.exe
+	./a.exe
+	
 yeux.txt : a.exe
 	./a.exe
 
